ae3dc67a7ce3869904f38eea557223c1295f512c
roshasjohnson <roshasj10@gmail.com>
2025-02-10 20:39:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple Ai chat bot
+
+This project is a simple AI-powered chatbot using FastAPI for the backend and HTML, CSS, and JavaScript for the frontend. The chatbot interacts with an AI model via API requests and displays responses dynamically.
+
+## Backend:
+    Python
+    FastAPI
+    Requests
+    Pydantic
+    dotenv (for environment variables)
+
+# Frontend:
+    HTML
+    CSS
+    JavaScript
+
+# Installation & Setup
+
+## Clone the repository
+git clone https://github.com/ai-chat-bot.git
+
+cd ai-chat-bot
+
---
+++ server.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+API_KEY = os.getenv("API_KEY")
+BASE_URL = os.getenv("BASE_URL")
+MODEL = os.getenv("MODEL")
+
+print(f"API_KEY: {API_KEY}")
+print(f"BASE_URL: {BASE_URL} ")
+print(f"MODEL: {MODEL}")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class ChatRequest(BaseModel):
+    message:str
+
+
+@app.post("/chat")
+async def chat(request: ChatRequest):
+    print(request.message)
+
+    client = OpenAI(
+        base_url=f"{BASE_URL}",
+        api_key=f"{API_KEY}",
+        )
+    
+    try:
+ 
+        completion = client.chat.completions.create(
+            model=f"{MODEL}",
+            messages=[
+                {
+                "role": "user",
+                "content": f"{request.message}"
+                }
+            ]
+            )
+        response = completion.choices[0].message.content
+        
+        return  {"reply":response}
+
+    except: 
+        response = "Error occured try again"
+        return {"reply":response}
